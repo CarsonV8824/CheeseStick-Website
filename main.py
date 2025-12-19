9222777678f4460cb233da5994788024a6800de3
CarsonV <166647418+CarsonV8824@users.noreply.github.com>
2025-12-19 13:20:02 -0600

    basic backend
    I made a basic backend with the file layout that flask docs suggested

---
+++ main.py
@@ -0,0 +1,42 @@
+from flask import Flask, jsonify, request, render_template
+
+app:Flask = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+#organizational structure of the project suggested by flask docs:
+"""
+/home/user/Projects/flask-tutorial
+├── flaskr/
+│   ├── __init__.py
+│   ├── db.py
+│   ├── schema.sql
+│   ├── auth.py
+│   ├── blog.py
+│   ├── templates/
+│   │   ├── base.html
+│   │   ├── auth/
+│   │   │   ├── login.html
+│   │   │   └── register.html
+│   │   └── blog/
+│   │       ├── create.html
+│   │       ├── index.html
+│   │       └── update.html
+│   └── static/
+│       └── style.css
+├── tests/
+│   ├── conftest.py
+│   ├── data.sql
+│   ├── test_factory.py
+│   ├── test_db.py
+│   ├── test_auth.py
+│   └── test_blog.py
+├── .venv/
+├── pyproject.toml
+└── MANIFEST.in
+"""
