d5e29ff2665846ac75da0985deb8cfeec5f620ed
CarsonV <166647418+CarsonV8824@users.noreply.github.com>
2025-12-20 19:09:20 +0000

    fixed database and added test cases in

--- src/database/database.py
+++ src/database/database.py
@@ -1,13 +1,40 @@
 import sqlite3
+import json
+
+#might use a different databse instead of sqlite later. This is a prototype of the database class
 
 class Database:
 
+    #constructor
     def __init__(self, database="CheeseStick.sqlite"):
         self.connection = sqlite3.connect(database)
         self.cursor = self.connection.cursor()
         self._make_table()
 
+    #Can change data in table later if needed
     def _make_table(self):
         self.cursor.execute("""
         CREATE TABLE IF NOT EXISTS CheeseStick
+        (id INTEGER PRIMARY KEY,
+        name TEXT,
+        quantity INTEGER)
     """)
+        self.connection.commit()
+    
+    #adds data to the database
+    def add_data(self,data):
+        self.cursor.execute("INSERT INTO CheeseStick (name, quantity) VALUES (?, ?)", data)
+        self.connection.commit()
+
+    #retrieves data from the database
+    def get_data(self):
+        self.cursor.execute("SELECT * FROM CheeseStick")
+        return self.cursor.fetchall()
+    
+    #the ? prevents SQL injection attacks
+    def get_certain_data(self, data_id):
+        self.cursor.execute("SELECT * FROM CheeseStick WHERE id=?", (data_id,))
+        return self.cursor.fetchone()
+    
+    def close(self):
+        self.connection.close()
--- tests/test_database.py
+++ tests/test_database.py
@@ -4,4 +4,30 @@ import pytest
 #this file will test all databases
 
 def test_get_data():
-    assert True
+    db = Database(":memory:")  # Use in-memory database for testing
+    db.add_data(("Cheddar", 10))
+    db.add_data(("Mozzarella", 20))
+    data = db.get_data()
+    assert len(data) == 2
+    assert data[0][1] == "Cheddar"
+    assert data[0][2] == 10
+    assert data[1][1] == "Mozzarella"
+    assert data[1][2] == 20
+    db.close()
+
+def test_get_certain_data():
+    db = Database(":memory:")
+    db.add_data(("Gouda", 15))
+    data = db.get_certain_data(1)
+    assert data[1] == "Gouda"
+    assert data[2] == 15
+    db.close()
+
+def test_add_data():
+    db = Database(":memory:")
+    db.add_data(("Brie", 5))
+    data = db.get_data()
+    assert len(data) == 1
+    assert data[0][1] == "Brie"
+    assert data[0][2] == 5
+    db.close()
