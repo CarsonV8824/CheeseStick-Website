24f2f731901e3199e8cd509f8114f566487f12ce
CarsonV <166647418+CarsonV8824@users.noreply.github.com>
2025-12-20 19:01:41 +0000

    added basic js and css in for testing. Also added file layout in

---
+++ src/static/script.js
@@ -0,0 +1,8 @@
+//this is for testing 
+
+const TestButton = document.getElementById("testing button");
+
+TestButton.addEventListener('click', () => {
+    console.log("button clicked");
+    alert("Button was clicked!");
+});
---
+++ tests/test_database.py
@@ -0,0 +1,7 @@
+from src.database.database import Database
+import pytest
+
+#this file will test all databases
+
+def test_get_data():
+    assert True
